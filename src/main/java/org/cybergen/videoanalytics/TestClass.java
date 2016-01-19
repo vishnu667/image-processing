ff5f2636d8359c5ee0e16417b4209341d6a30967
vishnu prasad <vishnu667@gmail.com>
2016-01-19 19:57:21 +0530

    wrap up commit

--- src/main/java/org/cybergen/videoanalytics/TestClass.java
+++ src/main/java/org/cybergen/videoanalytics/TestClass.java
@@ -1,18 +1,48 @@
 package org.cybergen.videoanalytics;
 
 import org.cybergen.videoanalytics.utils.ImageUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.image.BufferedImage;
 
 public class TestClass {
-	public static void main(String args[]){
+    private static final Logger log = LoggerFactory.getLogger(TestClass.class);
+    public static void main(String args[]){
 
         BufferedImage image1 = ImageUtils.getImageFromFile("src/test/resources/image1.png");
 
         BufferedImage image2 = ImageUtils.getImageFromFile("src/test/resources/image2.png");
 
-        ImageUtils.imageToGreyScale(image1);
-        ImageUtils.viewImage(image1);
-
+        BufferedImage greyImage1 = ImageUtils.imageToGreyScale(image1);
+        BufferedImage greyImage2 = ImageUtils.imageToGreyScale(image2);
+        int[][] data1 = ImageUtils.imageTo2D(greyImage1);
+        int[][] data2 = ImageUtils.imageTo2D(greyImage2);
+        log.info("height1 " + greyImage1.getHeight());
+        log.info("height2 " + greyImage2.getHeight());
+        log.info("width1 " + greyImage1.getWidth());
+        log.info("width2 " + greyImage2.getWidth());
+        compareImages(data1,data1);
 	}
+
+    public static void compareImages(int[][] imageA,int[][] imageB){
+        int widthA = imageA.length;
+        int widthB = imageB.length;
+        int heightA = imageA[0].length;
+        int heightB = imageB[0].length;
+        int[][] result = new int[heightA][widthA];
+
+        for(int i=0;i< widthA;i++){
+            for(int j=0 ; j<heightA ; j++){
+                if(imageA[i][j]==imageB[i][j]){
+                    result[i][j]= 1;
+                }else {
+                    result[i][j]= 0;
+                }
+
+            }
+            System.out.println(result[0][1]);
+        }
+    }
+
 }
--- src/main/java/org/cybergen/videoanalytics/utils/ImageUtils.java
+++ src/main/java/org/cybergen/videoanalytics/utils/ImageUtils.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 
@@ -46,6 +47,47 @@ public class ImageUtils {
         return newGreyImage;
     }
 
+    public static int[][] imageTo2D(BufferedImage image) {
 
+        int height = image.getHeight();
+        int width = image.getWidth();
+
+        boolean hasAlpha = image.getAlphaRaster() != null;
+        final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        int[][] result = new int[height][width];
+
+        if (hasAlpha) {
+            final int pixelLength = 4;
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+                argb += ((int) pixels[pixel + 1] & 0xff); // blue
+                argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+                result[row][col] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        } else {
+            final int pixelLength = 3;
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += -16777216; // 255 alpha
+                argb += ((int) pixels[pixel] & 0xff); // blue
+                argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+                result[row][col] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+        return result;
+    }
 
 }
